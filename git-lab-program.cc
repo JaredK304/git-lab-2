ca470a4bc477651a8917a22ed237d6c3e29b1f0e
Jared <jk783116@ohio.edu>
2020-09-04 17:51:49 -0400

    hello git

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jared Kennedy
+ *        Date: 9-4-2020
+ * Description: git-lab 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "hello Git!!" << endl;
+    return 0;
+}// main
